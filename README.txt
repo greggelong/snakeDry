7f80b1c5a5aaaf1572de4ece337e4f94d69e9b4b
Greg K <gkreisman_cep@qq.com>
2023-10-23 20:43:50 +0800

    first commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+# I feel I am still a snake 
+
+## a snake that has been tamed and changed into a hairdryer 
+
+a small sketch inspired by Artist Redjade Yuan
+
+https://www.instagram.com/yuanredjade/
+
+
+- [] need to make a loading screen for all the assets 
---
+++ sketch.js
@@ -0,0 +1,70 @@
+
+let hs
+let img
+let snake
+let playing =false
+let angle =0
+let goal = 180
+let cnv
+function preload(){
+  hs = loadSound("hds.mp3")
+  img = loadImage("hd.png")
+  snake = loadImage("snake.png")
+
+}
+
+function setup() {
+  // put setup code here
+  cnv = createCanvas(500,500)
+  let cx = floor((windowWidth - cnv.width) / 2);
+  let cy =  floor((windowHeight - cnv.height) / 2);
+  cnv.position(cx,cy)
+  background(0)
+  angleMode(DEGREES)
+  imageMode(CENTER)
+}
+
+function mousePressed(){
+  print(playing)
+  if(!playing){
+    hs.play()
+    playing = true
+  } else{
+    hs.stop()
+    playing = false
+  }
+}
+
+function draw() {
+  background(0)
+  push()
+  translate(width/2,height/2)
+  rotate(angle/3)
+  tint(180,212,148,(50+angle/3))
+  image(snake,0,0,500,500)
+  pop()
+  push()
+  translate(width/2,height/2)
+  rotate(angle)
+  tint(255)
+  image(img,0,0,350+(angle/2),250+angle/2)
+  pop()
+  if(playing){
+  if (angle<goal){
+    angle++
+    angle%360
+
+  }else if (angle>goal){
+     
+    if (angle>0){
+      angle--
+    }
+  }
+
+  else if (angle === goal){
+    goal=floor(random(360))
+
+  }
+}
+  
+}
